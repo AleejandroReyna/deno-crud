877b8ef039a0480b755e3020fa66bfcf631d2711
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-12-21 17:36:04 -0600

    Update moviesController.ts
    Added single route for movie

--- controllers/moviesController.ts
+++ controllers/moviesController.ts
@@ -4,11 +4,31 @@ import { Movie } from "../db/interfaces.ts"
 
 // Route for Get Movies 
 const getMovies = async ({ response }: { response : Response }) => {
-    let data : Movie[] = await Client.query("select * from movies")
+    const data : Movie[] = await Client.query("select * from movies;")
     response.body = {
         status: true,
         data
     };
 };
 
-export { getMovies }
+// Route for Get Movie
+const getMovie = async ({params, response} : {params : {id: string}, response : Response }) => {
+    const { id } = params
+    const data : Movie[] = await Client.query(
+        "select ??,name from ?? where id = ?",
+        ["*", "movies", id],
+        );
+    if(data.length) {    
+        response.body = {
+            status: true,
+            data: data[0]
+        }
+    } else {
+        response.status = 404
+        response.body = {
+            status: false
+        }
+    }
+}
+
+export { getMovies, getMovie }
