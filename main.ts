632db9177a863b1cc497910f4fed06387507b285
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-12-20 21:50:50 -0600

    Added dotenv

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const app = new Application();
-const port = 5000;
+const { PORT } = config();
+const port:number = Number(PORT);
 
 app.use(({ response }) => {
-    response.body = "First project with Deno (:";
+    response.body = `First project with Deno (: \nRunning in port: ${port}`;
 });
 
 await app.listen({ port });
