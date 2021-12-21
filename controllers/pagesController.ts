875f286161af2b416dc7395d160fc4ceeeaf5bca
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-12-21 00:10:53 -0600

    Update pagesController.ts
    Added response type for home method

--- controllers/pagesController.ts
+++ controllers/pagesController.ts
@@ -1,4 +1,6 @@
+import { Response } from "https://deno.land/x/oak/mod.ts";
+
 // home route
-export const home = (({ response } : {response: any}) => {
+export const home = (({ response } : {response : Response}) => {
     response.body = `API REST for movies with Deno (:`;
 });
