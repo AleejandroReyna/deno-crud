16f62ad65d6cbe872ec5076405529a0b8eb5d53f
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-12-20 21:55:13 -0600

    Update main.ts
    Added type for application

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-const app = new Application();
+const app : Application = new Application();
 const { PORT } = config();
 const port:number = Number(PORT);
 
