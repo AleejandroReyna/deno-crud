677c5182726701b83ae7e3e3674f58e80d4b1ce9
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-12-22 17:18:50 -0600

    Create depts.ts
    Added file with depts

---
+++ depts.ts
@@ -0,0 +1,15 @@
+// Oak
+export {
+    Application,
+    Response,
+    Request,
+    Router
+} from "https://deno.land/x/oak/mod.ts";
+
+// Dotenv
+export * as dotenv from "https://deno.land/x/dotenv/mod.ts";
+
+// mySQL
+export {
+    Client
+} from "https://deno.land/x/mysql/mod.ts";
