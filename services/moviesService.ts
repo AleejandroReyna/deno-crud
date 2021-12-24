5ed4f08424b8f07334baa662c5ce8a91d0a21b73
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-12-23 21:00:02 -0600

    Update moviesService.ts
    Added default item on moviesService

--- services/moviesService.ts
+++ services/moviesService.ts
@@ -1,9 +1,16 @@
 import {
     ModelService
 } from './classes.ts'
+import {
+    Movie
+} from '../db/interfaces.ts'
 
 class MoviesService extends ModelService {
     table : string = "movies"
+    defaultItem : Movie = {
+        name: "",
+        description: ""
+    }
 }
 
 export { MoviesService }
