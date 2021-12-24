ba2ec57855689e384e390a520c30bc1c78bee807
Alejandro Reyna <aleejandroreyna@gmail.com>
2021-12-23 20:59:47 -0600

    Update classes.ts
    Added createItem method on abstract ModelService and defaultItem without errors manage

--- services/classes.ts
+++ services/classes.ts
@@ -1,13 +1,15 @@
 import { 
     ModelInterface,
-    GetterModelInterface
+    GetterModelInterface,
+    CreatorModelInterface
 } from './interfaces.ts'
 import { 
     Client 
 } from "../db/client.ts"
 
-export abstract class ModelService implements GetterModelInterface {
+export abstract class ModelService implements GetterModelInterface, CreatorModelInterface {
     abstract table : string
+    abstract defaultItem : any
     
     async getItems() : Promise<any[]> {
         const data : any[] = await Client.query(`select * from ${this.table};`)
@@ -15,7 +17,21 @@ export abstract class ModelService implements GetterModelInterface {
     }
 
     async getItem(id : string | number ) : Promise<any> {
-        const data : any = await Client.query(`select * from ${this.table} where id = ?`, [id])
+        const data : any[] = await Client.query(`select * from ${this.table} where id = ?`, [id])
         return data.length ? data[0] : null
+    }
+
+    async createItem(item : any) : Promise<any> {
+        const requestObject = {...this.defaultItem, ...item}
+        const columns = Object.keys(requestObject)
+        const values = Object.values(requestObject)
+        const request = `INSERT INTO ${this.table} (${columns.join(', ')}) VALUES (${columns.map(_c => "?").join(", ")})`
+        const result = await Client.execute(request, values)
+        if(result) {
+            if(result.affectedRows && result.lastInsertId) {
+                return await this.getItem(result.lastInsertId)
+            }
+        }
+        return null
     }
 }
